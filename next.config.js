c1766db57bcf8ac1cb903adac5dcafc8429754b5
paraskumarmd <paraskumar.desh@gmail.com>
2025-08-16 03:50:57 +1200

    Optimize Next.js config for Cloudflare Pages bundle size

--- next.config.js
+++ next.config.js
@@ -4,6 +4,39 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  
+  // Cloudflare Pages optimization
+  experimental: {
+    optimizePackageImports: ['lucide-react'],
+  },
+  
+  // Reduce bundle size
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Split vendor chunks to reduce individual file sizes
+      config.optimization.splitChunks = {
+        chunks: 'all',
+        maxSize: 244000, // Just under 25MB limit
+        cacheGroups: {
+          default: {
+            minChunks: 1,
+            priority: -20,
+            reuseExistingChunk: true,
+          },
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            priority: -10,
+            chunks: 'all',
+          },
+        },
+      };
+    }
+    return config;
+  },
+  
+  // Enable compression
+  compress: true,
 };
 
 module.exports = nextConfig;
